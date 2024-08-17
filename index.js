993bedd020e826ab164f9ec43a4996d6928a2433
sanjuwabn4 <sanjuwaofc303@gmail.com>
2024-08-18 00:10:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
 fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
-console.log("Session downloaded ✅")
+console.log("Session Connected ✅")
 })})}
 
 const express = require("express");
