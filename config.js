20233ff13cdd8acbb37ef2ea629d991dec4e3af5
sanjuwabn4 <sanjuwaofc303@gmail.com>
2024-08-18 00:40:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG,|| "https://telegra.ph/file/c6c8452820c44d0b388eb.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG,|| "🚀•🎯 Hi I'am Online ♥️•🌐",
 };
